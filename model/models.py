6ba985f698b9f1d775e57ab964faf69b1ac8c046
梁荣 <2801293031@qq.com>
2018-11-07 14:25:18 +0800

    创建模型，数据迁移操作

--- model/models.py
+++ model/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Shop(models.Model):
+    # 设置id为主键，自增
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=15)
+    price = models.IntegerField()
+
+    # 解决显示不明确的项目标题
+    def __str__(self):
+        return self.name
