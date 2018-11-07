9362f61d1edb6bf20cf116a5d2bc246c2db1ed8f
梁荣 <2801293031@qq.com>
2018-11-07 20:39:05 +0800

    注册admin后台

--- model/admin.py
+++ model/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from model.models import *
+# 注册模型，显示在admin界面中，便于管理
 
-# Register your models here.
+
+# 注册student模型管理器
+@admin.register(Student)
+class Student_Admin(admin.ModelAdmin):
+    # 设置要显示在界面中的字段
+    list_display = ('id', 'name', 'sex', 'tel')
+    # 每页显示的条数
+    list_per_page = 10
+    # 根据id排序
+    ordering = ('id',)
+
+
+# 注册User模型
+@admin.register(Scoce)
+class Scoce_Admin(admin.ModelAdmin):
+    list_display = ('id', 'C', 'python', 'Django', 'Student')
+    ordering = ('id',)
